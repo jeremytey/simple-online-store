a00042ca5082cc086ffbec10fc316cfc36da45b7
jeremytey <jeremy8598@gmail.com>
2025-11-21 17:39:53 +0800

    finished app.js

--- js/app.js
+++ js/app.js
@@ -1,31 +1,30 @@
 //app.js — (Main entry point)
 import { fetchProducts } from './api.js';
 import * as router from './router.js';
+import { openCartModal } from './cart.js';
+import  { setProducts } from './products.js';
+
+function setupGlobalEvents() {
+    const btnViewProducts = document.querySelector('#btnViewProducts');
+    const btnCart = document.querySelector('#btnCart');
+    const btnBackToProducts = document.querySelector('#btnBackToProducts');
+    const btnCartSeeProducts = document.querySelector('#btnCartSeeProducts');
+
+    btnViewProducts.addEventListener('click', () => router.navigate("products"));
+    btnCart.addEventListener('click', openCartModal);
+    btnBackToProducts.addEventListener('click', () => router.navigate("products"));
+    btnCartSeeProducts.addEventListener('click', () => router.navigate("products"));
+}
 
 
-on DOM loaded:
-	initializeApp()
-    setupGlobalEvenets()
+function initializeApp(){
+    fetchProducts().then(list => {
+        setProducts(list);
+        router.navigate("landing");
+    });
+}
 
-
-
-function setupGlobalEvents():
-//attach button listener to all buttons
-    # Landing page → products
-    btnViewProducts.onclick = () => router.navigate("products")
-
-    # Header cart button
-    btnCart.onclick = openCartModal
-
-    # Product details back button
-    btnBackToProducts.onclick = () => router.navigate("products")
-
-    # Cart page "See more products"
-    btnCartSeeProducts.onclick = () => router.navigate("products")
-
-
-function initializeApp()
-	fetchProducts()
-	setProducts(list)
-    router.navigate("landing")
-
+document.addEventListener('DOMContentLoaded', () => {
+    initializeApp();
+    setupGlobalEvents();
+});
