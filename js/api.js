f9ae71be2f6abe8d7d6b81717fbd84a3ecd8fd32
Jeremy Tey Jie Ming <jeremy8598@gmail.com>
2025-11-22 23:25:41 +0800

    x

--- js/api.js
+++ js/api.js
@@ -5,7 +5,7 @@
         setTimeout(() => {
             resolve([
                 { id: 1, name: "Fidget Spinner", description: "Description of Product A", price: 29.99 },
-                { id: 2, name: "SECKSLOL", description: "Description of Product B", price: 49.99 },
+                { id: 2, name: "SOCKSLOL", description: "Description of Product B", price: 49.99 },
                 { id: 3, name: "Product C", description: "Description of Product C", price: 19.99 }
             ]); 
         }, 500); 
